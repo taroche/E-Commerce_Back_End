23a44128ab4c1d448e3975c30c7fc8bbaaeddecb
Tristan LaRoche <laroche11235@gmail.com>
2021-03-20 16:12:55 -0400

    delete route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -66,7 +66,16 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(categoryData => res.json(categoryData))
+    .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
