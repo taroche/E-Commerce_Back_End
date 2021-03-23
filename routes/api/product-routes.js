0ea23602fbe5f120817d2474accfa21da0a62ad2
Tristan LaRoche <laroche11235@gmail.com>
2021-03-23 13:24:22 -0400

    get route to find one

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -28,8 +28,25 @@ router.get('/', (req, res) => {
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+    }
+    ]
+  })
+    .then(productData => res.json(productData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // create new product
