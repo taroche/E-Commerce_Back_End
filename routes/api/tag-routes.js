b554cdf32c59fa4fad58b418b037a5490158370f
Tristan LaRoche <laroche11235@gmail.com>
2021-03-23 13:31:01 -0400

    route to delete tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -68,7 +68,22 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: 'No Tag found by that ID.' });
+        return;
+      }
+      res.json(tagData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
